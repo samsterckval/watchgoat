1bb88b0f459a1598c5a29e9a9561074f44bc7d11
Sam Sterckval <sam@edgise.com>
2021-10-27 14:28:50 +0200

    overhauled install script

--- install.sh
+++ install.sh
@@ -1,110 +1,149 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Installing WatchGoat"       # Just some random print to know it actually starts
 
-curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/main.py
-curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/systemd/watchgoat.service
-curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/systemd/watchgoat.timer
+printf "Welcome to watchgoat, let's install this motherfucker on your system."
+
+
+### What needs to be done?
+# * Download needed files
+# * Install netifaces
+# * Check and create needed directories
+# * Ask user for needed info
+# * Make executable
+# * Make executable, executable
+# * Replace 'PATHTOEXECUTABLE' where needed and copy services to the right place
+# * Start service
+
+
+
+### DOWNLOAD NEEDED FILES ###
+
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  printf "Running MacOS, going the launchd route"
+  curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/main.py
+  curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/launchd/com.samsterckval.watchgoat.plist
+elif [[ "$OSTYPE" == "linux"* ]]
+then
+  printf "Running Linux, going the systemd route"
+  curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/main.py
+  curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/systemd/watchgoat.service
+  curl -O https://raw.githubusercontent.com/samsterckval/watchgoat/main/systemd/watchgoat.timer
+fi
+
+
+
+### INSTALL NETIFACES ###
+
 
 sudo -H pip install netifaces
 
-BIN_DIR="bin"
+
+
+### CHECK DIRECTORIES ###
+
+BIN_DIR="$HOME/bin"
 EXEC_NAME="watchgoat"
-DEST="$HOME/$BIN_DIR/$EXEC_NAME"
-INFO_DIR=".watchgoat"
+EXEC_DEST="$BIN_DIR/$EXEC_NAME"
+INFO_DIR="$HOME/.watchgoat"
 URL_FILE="watchgoat_urls"
+URL_DEST="$INFO_DIR/$URL_FILE"
 SECRETS_FILE="watchgoat_secrets"
+SECRETS_DEST="$INFO_DIR/$SECRETS_FILE"
 
 
-if [ ! -d "$HOME/$BIN_DIR" ]
+if [ ! -d "$BIN_DIR" ]
 then
-    echo "Bin folder doesn't exist. Creating now"
-    mkdir "$HOME/$BIN_DIR"
-    echo "Folder created"
+    printf "Bin directory %s doesn't exist. Creating now" "$BIN_DIR"
+    mkdir "$BIN_DIR"
+    printf "Created"
 else
-    echo "Bin folder already exists"
+    printf "Bin directory %s already exists, did nothing yet" "$BIN_DIR"
 fi
 
-if [ ! -d "$HOME/$INFO_DIR" ]
+if [ ! -d "$INFO_DIR" ]
 then
-    echo "Info directory doesn't exist. Creating now"
-    mkdir "$HOME/$INFO_DIR"
-    echo "Folder created"
+    echo "Info directory %s doesn't exist. Creating now" "$INFO_DIR"
+    mkdir "$INFO_DIR"
+    echo "Created"
 else
-    echo "Info folder already exists"
+    echo "Info directory %s already exist, did nothing yet" "$INFO_DIR"
 fi
 
-if [ ! -f "$HOME/$INFO_DIR/$URL_FILE" ]
+
+
+### ASK USER FOR NEEDED INFO ###
+
+
+if [ ! -f "$URL_DEST" ]
 then
-    echo "Creating WatchGoat URL file with base URL"
-    echo "Where did you deploy the WatchGoat server?"
-    touch "$HOME/$INFO_DIR/$URL_FILE"
+    printf "Creating WatchGoat URL file @ %s" "$URL_DEST"
+    touch "$URL_DEST"
+    echo "Where did you deploy the WatchGoat server?  (e.g. https://mydomain.hosting.com/)"
     read -r BASEURL
-    printf "%s/goat" "$BASEURL" >> "$HOME/$INFO_DIR/$URL_FILE"
+    printf "%s/goat" "$BASEURL" >> "$URL_DEST"
 else
-    echo "WatchGoat URL file already exists, contents of it are not changed in any way"
+    echo "WatchGoat URL file @ %s already exists, contents of it are not changed in any way" "$URL_DEST"
 fi
 
-if [ ! -f "$HOME/$INFO_DIR/$SECRETS_FILE" ]
+if [ ! -f "$SECRETS_DEST" ]
 then
-    echo "Creating WatchGoat secrets file"
-    touch "$HOME/$INFO_DIR/$SECRETS_FILE"
-    echo "Username?"
-    echo "Tip : If this is a Raspberry Pi, an NXP i.MX8 or from the NVIDIA Jetson family, use 'rpi',"
-    echo "for a mac, use 'mac'. This will translate to the unique serial number of the device."
+    printf "Creating WatchGoat secrets file @ %s" "$SECRETS_DEST"
+    touch "$SECRETS_DEST"
+    printf "Username?"
+    printf "Tip : If this is a Raspberry Pi, an NXP i.MX8(unconfirmed) or from the NVIDIA Jetson family, use 'rpi',"
+    printf "for a device running MacOS, use 'mac'. This will translate to the unique serial number of the device."
+    printf "Else, just use a unique, recognisable name."
     read -r USERNAME
-    echo "Password?"
+    printf "Password?"
     read -r PASSWORD
-    printf "%s\n%s" "$USERNAME" "$PASSWORD" >> "$HOME/$INFO_DIR/$SECRETS_FILE"
+#    printf "IP getter method?"
+#    printf "For now, only 'netifaces' is supported, so this is a pseudo-choice, fuck you"
+#    read -r IPMETHOD
+    IPMETHOD="netifaces"
+    printf "%s\n%s\n%s" "$USERNAME" "$PASSWORD" "$IPMETHOD" >> "$SECRETS_DEST"
 else
-    echo "WatchGoat secrets file already exists, contents of it are not changed in any way"
+    echo "WatchGoat secrets file @ %s already exists, contents of it are not changed in any way" "$SECRETS_DEST"
 fi
 
+
+
+### MAKE EXECUTABLE ###
+
+sudo mv "main.py" "$EXEC_DEST"
+
+
+
+### MAKE EXECUTABLE, EXECUTABLE ###
+
+sudo chmod +x "$EXEC_DEST"
+
+
+### REPLACE PATH, COPY & START SERVICES ###
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  printf "Copying launchd service to %s/Library/LaunchAgents/" "$HOME"
+  sudo sed -i "s|PATHTOEXECUTABLE|$EXEC_DEST $URL_DEST $SECRETS_DEST|" "com.samsterckval.watchgoat.plist"
+  sudo mv "com.samsterckval.watchgoat.plist" "$HOME/Library/LaunchAgents/com.samsterckval.watchgoat.plist"
+  launchctl load com.samsterckval.watchgoat.plist
+elif [[ "$OSTYPE" == "linux"* ]]
+then
+  printf "Copying systemd service & timer"
+  sudo sed -i "s|PATHTOEXECUTABLE|$EXEC_DEST $URL_DEST $SECRETS_DEST|" "watchgoat.service"
+  sudo mv "watchgoat.server" "/etc/systemd/system/watchgoat.service"
+  sudo mv "watchgoat.timer" "/etc/systemd/system/watchgoat.timer"
+  sudo systemctl daemon-reload
+  sudo systemctl start watchgoat.timer
+  sudo systemctl enable watchgoat.timer
+fi
+
+
+# Not going to throw this away, might come in handy in a few commits
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
+#SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus e.g. /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-echo "Path to this script : $SCRIPTPATH"
-
-# Create symbolic link -- We don't do that anymore
-#ln -s "$SCRIPTPATH/main.py" "$DEST"
-
-# Move and rename the main python script
-sudo mv "main.py" "$DEST"
-
-# We don't do this either anymore
-#echo "Symbolic link created for $SCRIPTPATH/main.py @ $DEST"
-
-# Make the link executable
-chmod +x "$DEST"
-
-echo "Made $DEST executable"
-
-# Copy systemd services
-#sudo cp "$SCRIPTPATH/systemd/watchgoat.service" "/etc/systemd/system/watchgoat.service"
-#sudo cp "$SCRIPTPATH/systemd/watchgoat.timer" "/etc/systemd/system/watchgoat.timer"
-
-# These should be downloaded with curl
-sudo mv "watchgoat.server" "/etc/systemd/system/watchgoat.service"
-sudo mv "watchgoat.timer" "/etc/systemd/system/watchgoat.timer"
-
-echo "Copied files to /etc/systemd/system/"
-
-sudo sed -i "s|PATHTOEXECUTABLE|$DEST $HOME/$INFO_DIR/$URL_FILE $HOME/$INFO_DIR/$SECRETS_FILE|" "/etc/systemd/system/watchgoat.service"
-
-cat "/etc/systemd/system/watchgoat.service"
-
-# Reload deamons
-sudo systemctl daemon-reload
-echo "Reloaded systemctl"
-
-# Start it to be sure?
-sudo systemctl start watchgoat.timer
-echo "Started watchgoat"
-
-# Enable timer by default
-sudo systemctl enable watchgoat.timer
-echo "Enabled watchgoat"
+#SCRIPTPATH=$(dirname "$SCRIPT")
 
 
