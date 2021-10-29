e63a203a0d683619ace0914d3254e274aaaed72c
Sam Sterckval <sam@edgise.com>
2021-10-29 14:15:45 +0200

    added uninstall

---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+
+### UNINSTALL ###
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  printrf "Running MacOS\n"
+  launchctl unload "$HOME/Library/LaunchAgents/com.samsterckval.watchgoat.plist"
+  printf "Unloaded launchd service @ %s\n" "$HOME/Library/LaunchAgents/com.samsterckval.watchgoat.plist"
+  sudo rm "$HOME/Library/LaunchAgents/com.samsterckval.watchgoat.plist"
+  printf "Removed launchd file\nAll done, bye.\n(PS, the watchgoat executable still exists @ ~/bin/watchgoat)\n"
+  exit 0
+elif [[ "$OSTYPE" == "linux"* ]]
+then
+  printf "Running Linux\n"
+  systemctl disable watchgoat.timer
+  systemctl stop watchgoat.timer
+  prinf "Stopped the systemd service\n"
+  sudo rm "/etc/systemd/system/watchgoat.service"
+  sudo rm "/etc/systemd/system/watchgoat.timer"
+  printf "Removed the systemd files, bye.\n(PS, the watchgoat executable still exists @ ~/bin/watchgoat)\n"
+  exit 0
+fi
